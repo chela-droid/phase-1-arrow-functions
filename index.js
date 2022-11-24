dcaf40f942895ca9ae1800a357d2391c0ba482bb
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-24 14:25:11 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(){
+
+}
+function divide(num1,num2){
+    return( 2000/100)
+    
+// }
+//  function square (){
+
+ }
+ let x = 2
+ const square = function ( x){return x*x}
+
+
+ let a=3
+ let b=4
+const add =function(a,b){return 3+4}
